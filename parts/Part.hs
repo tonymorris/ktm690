eee52552dd424fed2d1a866618fde8d5198763da
Tony Morris <tmorris@tmorris.net>
2014-06-27 19:30:35 +1000

    parts

--- parts/Part.hs
+++ parts/Part.hs
@@ -1,3 +1,10 @@
+import Data.Set(Set)
+
+data Parts =
+  Parts
+    [Part]
+  deriving (Eq, Ord, Show)
+
 data Part =
   Part
     String -- name
@@ -5,12 +12,21 @@ data Part =
     String -- vendor
     String -- URL
     Price
-    
+    Tags
+  deriving (Eq, Ord, Show)
+
+data Tags =
+  Tags
+    (Set String)
+  deriving (Eq, Ord, Show)
+
 data Price =
   Price
     Int -- cents
     Currency
-
+  deriving (Eq, Ord, Show)
+  
 data Currency =
   USD
   | AUD
+  deriving (Eq, Ord, Show)
